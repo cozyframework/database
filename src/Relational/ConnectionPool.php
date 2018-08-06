dae9fed0ed01198a9af13bd778bbe0c1671fb22f
Nestor Picado <info@nestorpicado.com>
2018-08-06 01:16:48 -0500

    Improve the way to handle the connections of a pool.

--- src/Relational/ConnectionPool.php
+++ src/Relational/ConnectionPool.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Cozy\Database\Relational;
 
-use Cozy\Database\Relational\Configuration\ConfigurationInterface;
+use Cozy\Database\Relational\Exceptions\Exception;
 
 class ConnectionPool
 {
@@ -21,48 +21,43 @@ class ConnectionPool
         $this->selectionOrder = $selection_order;
     }
 
-    public function addConfiguration(ConfigurationInterface $configuration, string $tag = 'main')
+    public function addConnection(Connection $connection, string $tag = 'main')
     {
         if (!isset($this->pool[$tag])) {
             $this->pool[$tag] = [];
         }
 
-        $this->pool[$tag][] = $configuration;
+        $this->pool[$tag][] = $connection;
     }
 
     public function getConnection(string $tag = 'main'): Connection
     {
-        if (isset($this->current[$tag]) && $this->current[$tag] instanceof Connection) {
-            if ($this->current[$tag]->isAlive()) {
-                return $this->current[$tag];
-            } else {
-                $this->current[$tag] = null;
-            }
+        /** @var Connection $connection */
+
+        if (isset($this->current[$tag]) && $this->current[$tag]->isAlive()) {
+            return $this->current[$tag];
         }
 
+        $this->current[$tag] = null;
+
         if (empty($this->pool[$tag])) {
-            throw new Exception('There are no available configurations in the connection pool.', 'CZ097');
+            throw new Exception('There are no available connections in the pool.', 'CZ097');
         }
 
         if ($this->selectionOrder == self::SELECTION_RANDOM) {
             shuffle($this->pool[$tag]);
         }
 
-        $configuration = array_shift($this->pool[$tag]);
-
-        while ($configuration !== null) {
-            if ($configuration->isValid()) {
-                $this->current[$tag] = $configuration->buildConnection();
-                break;
+//        $connection = array_shift($this->pool[$tag]);
+        while (null !== ($connection = array_shift($this->pool[$tag]))) {
+            try {
+                if ($connection->isAlive()) {
+                    return $this->current[$tag] = $connection;
+                }
+            } catch (\Throwable $e) {
             }
-
-            $configuration = array_shift($this->pool[$tag]);
         }
 
-        if (!isset($this->current[$tag])) {
-            throw new Exception('There are no valid configurations to build a database connection.', 'CZ098');
-        }
-
-        return $this->current[$tag];
+        throw new Exception('There are no live connections available.', 'CZ098');
     }
 }
